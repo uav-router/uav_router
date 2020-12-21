d7e5d037998afef935d579c737d90d98030772c1
uav-router <uav.router@gmail.com>
2020-12-21 19:18:04 +0200

    general broadcast examples

--- others/udpclient.py
+++ others/udpclient.py
@@ -1,12 +1,18 @@
 import socket
 msgFromClient       = "Hello UDP Server"
 bytesToSend         = str.encode(msgFromClient)
-serverAddressPort   = ("localhost", 20001)
+serverAddressPort   = ("192.168.4.118", 20001)
+#serverAddressPort   = ("", 20001)
 bufferSize          = 1024
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
 # Send to server using created UDP socket
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+#UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+#UDPClientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+UDPClientSocket.sendto(bytesToSend, ("192.168.4.118", 20001))
+
 msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 msg = "Message from Server {}".format(msgFromServer[0])
 print(msg)
--- others/udpserver.py
+++ others/udpserver.py
@@ -1,55 +1,25 @@
 import socket
-
- 
-
-localIP     = "localhost"
-
+localIP     = "192.168.4.118"
 localPort   = 20001
-
 bufferSize  = 1024
-
- 
-
 msgFromServer       = "Hello UDP Client"
-
 bytesToSend         = str.encode(msgFromServer)
-
- 
-
 # Create a datagram socket
-
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
- 
-
 # Bind to address and ip
+#UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 UDPServerSocket.bind((localIP, localPort))
-
- 
-
 print("UDP server up and listening")
-
- 
-
 # Listen for incoming datagrams
 
 while(True):
-
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-
     message = bytesAddressPair[0]
-
     address = bytesAddressPair[1]
-
     clientMsg = "Message from Client:{}".format(message)
     clientIP  = "Client IP Address:{}".format(address)
-    
     print(clientMsg)
     print(clientIP)
-
-   
-
     # Sending a reply to client
-
     UDPServerSocket.sendto(bytesToSend, address)
